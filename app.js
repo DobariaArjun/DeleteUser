aefebab259dbad9db3e533d10db2b5b033bbb2a4
Arjun Dobaria <arjundobaria@MacBook-Pro.local>
2018-09-21 20:03:56 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const admin = require('firebase-admin');
+const express = require('express'),
+    bodyParser = require('body-parser');
+const app = express(),
+    port = process.env.PORT || 3000;
+
+var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+var serviceAccount = require('./petra-fb42d-firebase-adminsdk-1l4nv-f5d20ad5b7.json');
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://petra-fb42d.firebaseio.com'
+});
+
+app.get('/',(req,res)=>{
+    res.send("Server Running... :) ");
+})
+
+app.post('/deleteUser',jsonParser,(req,res) => {
+    if (!req.body.uid) return res.status(400).send({"status" : "Bad Request"});
+
+    var uid = req.body.uid;
+
+    admin.auth().deleteUser(uid).then((userRecord) => {
+        return res.status(200).send({"status" : "User Deleted"})
+    }).catch((error) => {
+        return res.status(200).send({"status" : error.description});
+    })
+})
+
+app.listen(port);
+console.log('todo list RESTful API server started on: ' + port);
